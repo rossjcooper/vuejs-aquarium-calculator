445b90f77d31a008c8e10602e3826028a67ba1d4
Ross Cooper <ross.cooper@teknet.org.uk>
2017-04-27 10:44:47 +0100

    Restyled and adding google analytics

--- src/js/app.js
+++ src/js/app.js
@@ -27,32 +27,5 @@ var app = new Vue({
         pounds: function () {
             return this.kilos * 2.20462;
         }
-    },
-    directives: {
-        select: {
-            twoWay: true,
-
-            bind: function () {
-                $(this.el).material_select();
-
-                var self = this;
-
-                $(this.el).on('change', function () {
-                    self.set($(self.el).val());
-                });
-            },
-
-            update: function (newValue, oldValue) {
-                $(this.el).val(newValue);
-            },
-
-            unbind: function () {
-                $(this.el).material_select('destroy');
-            }
-        }
     }
-});
-
-$(document).ready(function () {
-    $('select').material_select();
 });
